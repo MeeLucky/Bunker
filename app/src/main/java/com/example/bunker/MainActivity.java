76d925af73f46fe8c022864f3db512941be2ec1c
MeeLock <zalipatr1@yandex.ru>
2020-06-19 06:01:27 +0300

    fullscreen and hide actionbar

--- app/src/main/java/com/example/bunker/MainActivity.java
+++ app/src/main/java/com/example/bunker/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.content.Intent;
 import android.widget.Button;
@@ -18,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
     }
 
     Bunker bunker;
